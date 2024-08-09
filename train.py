6e58999ac6618fa736d1e55fedd5513b98e98f7c
MohammadRezaShafie <mr.shafie7731@gmail.com>
2024-08-09 11:58:22 +0330

    back to old train script

--- train.py
+++ train.py
@@ -7,13 +7,13 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data as data
 import torch.backends.cudnn as cudnn
+import math
 import numpy as np
 from config import get_train_config
 from data import ModelNet40
 from models import MeshNet
 from utils.retrival import append_feature, calculate_map
-from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import f1_score
+
 
 cfg = get_train_config()
 os.environ['CUDA_VISIBLE_DEVICES'] = cfg['cuda_devices']
@@ -25,15 +25,22 @@ os.environ['PYTHONHASHSEED'] = str(seed)
 np.random.seed(seed)
 torch.manual_seed(seed)
 torch.cuda.manual_seed(seed)
-torch.cuda.manual_seed_all(seed)
+torch.cuda.manual_seed_all(seed) 
 
 # dataset
-dataset = ModelNet40(cfg=cfg['dataset'], part='train')
-test_set = ModelNet40(cfg=cfg['dataset'], part='test')
+data_set = {
+    x: ModelNet40(cfg=cfg['dataset'], part=x) for x in ['train', 'test']
+}
+data_loader = {
+    x: data.DataLoader(data_set[x], batch_size=cfg['batch_size'], shuffle=True)
+    for x in ['train', 'test']
+}
 
-def train_model(model, criterion, optimizer, scheduler, train_loader, val_loader, cfg):
 
-    best_f1 = 0.0
+def train_model(model, criterion, optimizer, scheduler, cfg):
+
+    best_acc = 0.0
+    best_map = 0.0
     best_model_wts = copy.deepcopy(model.state_dict())
 
     for epoch in range(1, cfg['max_epoch']):
@@ -42,19 +49,19 @@ def train_model(model, criterion, optimizer, scheduler, train_loader, val_loader
         print('Epoch: {} / {}'.format(epoch, cfg['max_epoch']))
         print('-' * 60)
 
-        for phase, loader in [('train', train_loader), ('val', val_loader)]:
+        # adjust_learning_rate(cfg, epoch, optimizer)
+        for phrase in ['train', 'test']:
 
-            if phase == 'train':
+            if phrase == 'train':
                 model.train()
             else:
                 model.eval()
 
             running_loss = 0.0
             running_corrects = 0
-            all_preds = []
-            all_targets = []
+            ft_all, lbl_all = None, None
 
-            for centers, corners, normals, neighbor_index, targets in loader:
+            for i, (centers, corners, normals, neighbor_index, targets) in enumerate(data_loader[phrase]):
                 centers = centers.cuda()
                 corners = corners.cuda()
                 normals = normals.cuda()
@@ -63,50 +70,71 @@ def train_model(model, criterion, optimizer, scheduler, train_loader, val_loader
 
                 targets = targets.view(-1, 1)
 
-                with torch.set_grad_enabled(phase == 'train'):
-                    outputs, _ = model(centers, corners, normals, neighbor_index)
+                with torch.set_grad_enabled(phrase == 'train'):
+                    outputs, feas = model(centers, corners, normals, neighbor_index)
+                    _, preds = torch.max(outputs, 1)
+
                     preds = (torch.sigmoid(outputs) > 0.5).float()
+                    # print("Outputs:", outputs)
+                    # print("Targets:", targets)
+                    # print("Predictions:", preds)
                     loss = criterion(outputs, targets)
+                    # print("Loss:", loss.item())
                     
-                    if phase == 'train':
+                    if phrase == 'train':
                         optimizer.zero_grad()
                         loss.backward()
                         optimizer.step()
+
+                    if phrase == 'test' and cfg['retrieval_on']:
+                        ft_all = append_feature(ft_all, feas.detach().cpu())
+                        lbl_all = append_feature(lbl_all, targets.detach().cpu(), flaten=True)
+
                     running_loss += loss.item() * centers.size(0)
                     running_corrects += torch.sum(preds == targets.data)
-                    all_preds.append(preds.cpu().numpy())
-                    all_targets.append(targets.cpu().numpy())
-            print(f'{running_loss=}')
-            epoch_loss = running_loss / len(loader.dataset)
-            epoch_acc = running_corrects.double() / len(loader.dataset)
-            all_preds = np.concatenate(all_preds)
-            all_targets = np.concatenate(all_targets)
-            epoch_f1 = f1_score(all_targets, all_preds)
 
-            if phase == 'train':
-                print('{} Loss: {:.4f} Acc: {:.4f} F1: {:.4f}'.format(phase, epoch_loss, epoch_acc, epoch_f1))
+            epoch_loss = running_loss / len(data_set[phrase])
+            epoch_acc = running_corrects.double() / len(data_set[phrase])
+            # print('in train phase {}'.format(epoch_acc))
+
+            if phrase == 'train':
+                print('{} Loss: {:.4f} Acc: {:.4f}'.format(phrase, epoch_loss, epoch_acc))
                 scheduler.step()
 
-            if phase == 'val':
-                print('{} Loss: {:.4f} Acc: {:.4f} F1: {:.4f} (best F1 {:.4f})'.format(phase, epoch_loss, epoch_acc, epoch_f1, best_f1))
-                if epoch_f1 > best_f1:
-                    best_f1 = epoch_f1
+            if phrase == 'test':
+                # print('in train phase {}'.format(best_acc))
+                # print('in test phase {}'.format(epoch_acc))
+                if epoch_acc > best_acc:
+                    best_acc = epoch_acc
                     best_model_wts = copy.deepcopy(model.state_dict())
+                print_info = '{} Loss: {:.4f} Acc: {:.4f} (best {:.4f})'.format(phrase, epoch_loss, epoch_acc, best_acc)
 
+                if cfg['retrieval_on']:
+                    epoch_map = calculate_map(ft_all, lbl_all)
+                    if epoch_map > best_map:
+                        best_map = epoch_map
+                    print_info += ' mAP: {:.4f}'.format(epoch_map)
+                
                 if epoch % cfg['save_steps'] == 0:
                     torch.save(copy.deepcopy(model.state_dict()), os.path.join(cfg['ckpt_root'], '{}.pkl'.format(epoch)))
+                
+                print(print_info)
 
-    print('Best val F1: {:.4f}'.format(best_f1))
+    print('Best val acc: {:.4f}'.format(best_acc))
     print('Config: {}'.format(cfg))
 
     return best_model_wts
 
+
 if __name__ == '__main__':
+
     # prepare model
     model = MeshNet(cfg=cfg['MeshNet'], require_fea=True)
+    # model.cuda()
     model = nn.DataParallel(model)
 
     # criterion
+    # criterion = nn.CrossEntropyLoss()
     criterion = nn.BCEWithLogitsLoss()
 
     # optimizer
@@ -121,43 +149,8 @@ if __name__ == '__main__':
     else:
         scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=cfg['max_epoch'])
 
-    # start training with Stratified K-Fold Cross Validation
-    skf = StratifiedKFold(n_splits=4, shuffle=True, random_state=seed)
-    targets = np.array([dataset[i][4].item() for i in range(len(dataset))])
-
-    for fold, (train_idx, val_idx) in enumerate(skf.split(np.zeros(len(targets)), targets)):
-        print(f'Fold {fold+1}')
-        train_sampler = data.SubsetRandomSampler(train_idx)
-        val_sampler = data.SubsetRandomSampler(val_idx)
-
-        train_loader = data.DataLoader(dataset, batch_size=cfg['batch_size'], sampler=train_sampler)
-        val_loader = data.DataLoader(dataset, batch_size=cfg['batch_size'], sampler=val_sampler)
-
-        best_model_wts = train_model(model, criterion, optimizer, scheduler, train_loader, val_loader, cfg)
-        torch.save(best_model_wts, os.path.join(cfg['ckpt_root'], f'MeshNet_best_fold{fold+1}.pkl'))
-
-    # Evaluate the model on the test set
-    test_loader = data.DataLoader(test_set, batch_size=cfg['batch_size'], shuffle=False)
-    model.load_state_dict(best_model_wts)
-    model.eval()
-    all_preds = []
-    all_targets = []
-
-    with torch.no_grad():
-        for centers, corners, normals, neighbor_index, targets in test_loader:
-            centers = centers.cuda()
-            corners = corners.cuda()
-            normals = normals.cuda()
-            neighbor_index = neighbor_index.cuda()
-            targets = targets.cuda()
-
-            targets = targets.view(-1, 1)
-            outputs, _ = model(centers, corners, normals, neighbor_index)
-            preds = (torch.sigmoid(outputs) > 0.5).float()
-            all_preds.append(preds.cpu().numpy())
-            all_targets.append(targets.cpu().numpy())
-
-    all_preds = np.concatenate(all_preds)
-    all_targets = np.concatenate(all_targets)
-    test_f1 = f1_score(all_targets, all_preds)
-    print(f'Test F1 Score: {test_f1:.4f}')
+    # start training
+    if not os.path.exists(cfg['ckpt_root']):
+        os.mkdir(cfg['ckpt_root'])
+    best_model_wts = train_model(model, criterion, optimizer, scheduler, cfg)
+    torch.save(best_model_wts, os.path.join(cfg['ckpt_root'], 'MeshNet_best.pkl'))
